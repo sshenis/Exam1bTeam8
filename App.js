32da1dbcd0419ad6ad7f14b9aa54b5540d0b948f
Shenis Osias <shenisosias@gmail.com>
2019-10-22 09:41:09 -0700

    HELP ON SHOWING IMAGE
    So everything works up until you put a url in the input and you click Set Image. Please help.

--- App.js
+++ App.js
@@ -11,10 +11,10 @@ function App(){
 
     <View style={styles.app}>
 
-      <Main/>
+      <Main />
 
     </View>
-    
+
   )}
 
 
---
+++ comps/Left.js
@@ -0,0 +1,75 @@
+import React, {useState} from 'react';
+import {View, Text, TextInput, Button, Alert, Image} from 'react-native';
+import styles from '../styles/LeftStyles';
+ 
+ 
+function Left(){
+
+  // Input
+  const [titleText, setTitleText] = useState("");
+
+  // currently has no functionality
+  const [showImage, setShowImage] = useState(false);
+  
+  // Button
+  var but = null;
+
+  if (titleText == "") {
+    but = (
+    <View>
+    <Button
+    style = {styles.button}
+    title = "Set Image"
+    onPress={() => Alert.alert(
+
+     // TITLE OF ALERT
+   'Alert',
+
+   // THE VALUE OF THE INPUT
+   'Dont leave the text input blank!',
+
+   // OK BUTTON
+   [{text: 'OK', onPress: () => console.log('OK Pressed')}],
+
+   
+   {cancelable: false}
+   )} 
+   />
+   </View>
+    )
+  } 
+  // currently is not working but above is working
+  else {
+    but = (
+      <View>
+      <Button
+      style = {styles.button}
+      title = "Set Image"
+      onPress={() => setShowImage(titleText)} 
+     />
+     </View>
+      )
+
+  }
+
+
+
+
+  return (
+
+    <View style={styles.left}>
+
+      {but}
+
+      <TextInput 
+      style={styles.input}
+      placeholder = "image url" 
+      onChangeText = { text => setTitleText(text)}
+      />
+
+      <Text style={styles.title}>Your image url is {titleText}</Text>
+    </View>
+
+  )}
+
+export default Left;
