9efb9bedc0d92e18feb125c70534aa6c486fe39d
Shenis Osias <shenisosias@gmail.com>
2019-10-22 11:49:41 -0700

    Updating file

--- comps/Left.js
+++ comps/Left.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput, Button, Alert, Image} from 'react-native';
+import {View, Text, TextInput, Button, Alert, Image, ImageBackground} from 'react-native';
 import styles from '../styles/LeftStyles';
  
  
@@ -13,7 +13,6 @@ function Left(){
   
   // Button
   var but = null;
-  var bgImg = null;
 
   
   if (titleText == "") {
@@ -46,10 +45,12 @@ function Left(){
 
     <View style={styles.left}>
 
-      <Image
-      style={{width: '100%', height: 100, zIndex: 1000}}
-      source = {{uri: ' https://cdn.shopify.com/s/files/1/3004/1474/products/orange-tabby_1800x1800.png?v=1544042837'}}
-      />
+
+  <ImageBackground
+      style={[styles.left, {width: '100%', height: "100%"}]}
+      source={{uri: + " ' "+ {titleText} + " ' "}}
+        
+  >
 
       {but}
 
@@ -60,6 +61,7 @@ function Left(){
       />
 
       <Text style={styles.title}>Your image url is {titleText}</Text>
+      </ImageBackground>
     </View>
 
   )
