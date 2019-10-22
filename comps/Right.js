1cb9a6a61f72caaec6de2d5a3a0f1e9931848f3d
jjanicefong <janicefong11@gmail.com>
2019-10-22 11:54:12 -0700

    final push

--- comps/Right.js
+++ comps/Right.js
@@ -6,36 +6,39 @@ import styles from '../styles/RightStyles';
 
 function Right(){
 
-   const [setWidth, widthMode] = useState(false);
    var normalState = "40%";
+   const [setWidth, widthMode] = useState(false);
   // UI - ONLY USE MAIN TO CHANGE INTERFACE 
   // REMINDER YOU ARE CODING IN YOUR OWN BRANCH NOT MASTER
    if (setWidth === false) {
-      normalState = "50%"
+      normalState = "40%"
    } else {
-      normalState 
-   }
+      normalState = "50%"
+   } 
   
   return (
 
-    <View style={styles.right}>
+    <View style={[styles.right, {width: normalState}]}>
       <Text>Normal</Text>
       <Switch  
-         // onValueChange = {(value=>widthMode(value))}
-         // value = {setWidth}
+         value = {true}
       />
 
       <Text>Wide</Text>
       <Switch 
-         onValueChange = {widthMode}
+         onValueChange = {(value)=>widthMode(value)}
          value = {setWidth}
       />
 
-      {/* <Text>Wider</Text>
-      <Switch />
+      <Text>Wider</Text>
+      <Switch
+
+      />
 
       <Text>Widest</Text>
-      <Switch /> */}
+      <Switch 
+
+      />
 
     </View>
 
